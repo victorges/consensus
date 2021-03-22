a6e33c6c8044fe7c86a1f251061918392f8d3bbb
Victor Elias <victorgelias@gmail.com>
2021-03-22 03:36:49 -0300

    Map of transaction believers and some smart attempts

--- CompliantNode.java
+++ CompliantNode.java
@@ -15,9 +15,9 @@ public class CompliantNode implements Node {
     private int _numNodes;
     private final Set<Integer> _followees = new TreeSet<>();
 
-    private final Set<Transaction> _pendingTransactions = new TreeSet<>(s_transactionComparator);
-    private final Set<Transaction> _consensusTransactions = new TreeSet<>(s_transactionComparator);
+    private Set<Transaction> _pendingTransactions;
 
+    private final Map<Transaction, NodesSet> _transactionBelievers = new TreeMap<>(s_transactionComparator);
     private final Set<Integer> _maliciousNodes = new TreeSet<>();
     private int[] _followeeTransactions;
 
@@ -41,59 +41,128 @@ public class CompliantNode implements Node {
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        _pendingTransactions.clear();
-        _pendingTransactions.addAll(pendingTransactions);
+        _pendingTransactions = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
-        Set<Transaction> toSend = new TreeSet<>(s_transactionComparator);
-        toSend.addAll(_consensusTransactions);
-        if (_currRound++ >= _numRounds) {
-            return toSend;
+        if (_currRound++ < _numRounds) {
+            return _pendingTransactions;
         }
 
-        toSend.addAll(_pendingTransactions);
-        return toSend;
+        Set<Transaction> consensus = new TreeSet<>(s_transactionComparator);
+        int threshold = (int)((_followees.size()-_maliciousNodes.size())*0.7);
+        for (Transaction transaction : _transactionBelievers.keySet()) {
+            NodesSet nodes = _transactionBelievers.get(transaction);
+            if (nodes.roundCount() > threshold) {
+                consensus.add(transaction);
+            }
+        }
+        return consensus;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        Map<Transaction, Integer> believers = new TreeMap<>(s_transactionComparator);
-        _pendingTransactions.forEach(tx -> believers.put(tx, 1));
-        _consensusTransactions.forEach(tx -> believers.put(tx, 1));
-
+        _transactionBelievers.values().forEach(ns -> ns.bumpRound(_currRound));
         int[] nextFolloweeTransactions = new int[_numNodes];
         for (Candidate candidate : candidates) {
             if (!_followees.contains(candidate.sender)) continue;
-
             _pendingTransactions.add(candidate.tx);
-            if (_maliciousNodes.contains(candidate.sender)) continue;
 
             nextFolloweeTransactions[candidate.sender]++;
 
-            Integer count = believers.get(candidate.tx);
-            believers.put(candidate.tx, count == null ? 1 : count + 1);
+            if (_maliciousNodes.contains(candidate.sender)) continue;
+
+            if (!_transactionBelievers.containsKey(candidate.tx)) {
+                _transactionBelievers.put(candidate.tx, new NodesSet(_numNodes));
+            }
+            _transactionBelievers.get(candidate.tx).flagNode(candidate.sender);
         }
-        detectMaliciousNodes(nextFolloweeTransactions, candidates);
+
+        detectMaliciousNodes(nextFolloweeTransactions);
         _followeeTransactions = nextFolloweeTransactions;
 
-        int threshold = (int)((1+_followees.size()-_maliciousNodes.size())*0.5);
-        for (Transaction transaction : believers.keySet()) {
-            int count = believers.get(transaction);
-            if (count > threshold) {
-                _consensusTransactions.add(transaction);
+        candidates.clear();
+        for (Transaction tx : _pendingTransactions) {
+            if (!_transactionBelievers.containsKey(tx)) continue;
+
+            NodesSet nodes = _transactionBelievers.get(tx);
+            if (nodes.roundCount() <= 1) {
+                _transactionBelievers.remove(tx);
             }
         }
     }
 
-    private void detectMaliciousNodes(int[] nextFolloweeTransactions, Set<Candidate> candidates) {
+    private void detectMaliciousNodes(int[] nextFolloweeTransactions) {
         if (_currRound <= 1) return;
+
+        for (Transaction tx : _transactionBelievers.keySet()) {
+            NodesSet nodes = _transactionBelievers.get(tx);
+
+            int peaceBelievers = 0;
+            for (int nodeId : _followees) {
+                if (_maliciousNodes.contains(nodeId) || !nodes.everFlagged(nodeId)) continue;
+
+                if (!nodes.flaggedInRound(nodeId)) {
+                    _maliciousNodes.add(nodeId);
+                } else {
+                    peaceBelievers++;
+                }
+            }
+
+            int distrustTreshold = (int)((_followees.size()-_maliciousNodes.size())*0.9);
+            if (_currRound > 2*_numRounds/3 && peaceBelievers > distrustTreshold) {
+                for (int nodeId : _followees) {
+                    if (_maliciousNodes.contains(nodeId)) continue;
+
+                    if (!nodes.everFlagged(nodeId)) {
+                        _maliciousNodes.add(nodeId);
+                    }
+                }
+            }
+        }
 
         for (int nodeId : _followees) {
             boolean decreasingNumberOfTxs = nextFolloweeTransactions[nodeId] < _followeeTransactions[nodeId];
-            boolean returningNoTxs = _currRound > _numRounds/2 && _followeeTransactions[nodeId] == 0;
-            if (decreasingNumberOfTxs || returningNoTxs) {
+            boolean returningNoTxs = _currRound >= 3 && nextFolloweeTransactions[nodeId] == 0;
+            boolean returningOnlyOwnTxs = _currRound > _numRounds/2 && nextFolloweeTransactions[nodeId] <= 500*_p_txDistribution;
+            if (decreasingNumberOfTxs || returningNoTxs || returningOnlyOwnTxs) {
                 _maliciousNodes.add(nodeId);
             }
         }
+    }
+}
+
+class NodesSet {
+    public final byte[] _nodesLastRound;
+    private byte _currRound;
+    private int _currRoundCount;
+
+    public NodesSet(int numNodes) {
+        this._nodesLastRound = new byte[numNodes];
+    }
+
+    public void bumpRound(int round) {
+        if (round <= 0) throw new IllegalArgumentException("round must be positive");
+        if ((int)(byte)round != round) throw new IllegalArgumentException("round must fit a byte");
+        _currRound = (byte) round;
+        _currRoundCount = 0;
+    }
+
+    public void flagNode(int nodeId) {
+        if (_nodesLastRound[nodeId] == _currRound) return;
+
+        _nodesLastRound[nodeId] = _currRound;
+        _currRoundCount++;
+    }
+
+    public boolean flaggedInRound(int nodeId) {
+        return _nodesLastRound[nodeId] == _currRound;
+    }
+
+    public boolean everFlagged(int nodeId) {
+        return _nodesLastRound[nodeId] > 0;
+    }
+
+    public int roundCount() {
+        return _currRoundCount;
     }
 }
